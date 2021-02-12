e144f2f65438db2f511159f7382eefc39ca194d9
adrien.durmier <adrien.durmier@amediasolutions.fr>
2021-02-12 23:50:28 +0100

    front card

--- src/Twig/Planeswalkers/LegalityExtension.php
+++ src/Twig/Planeswalkers/LegalityExtension.php
@@ -18,16 +18,16 @@ class LegalityExtension extends AbstractExtension
         $response = '';
         switch ($card) {
             case "not_legal":
-                $response = '<i class="fas py-3 fa-times text-danger"></i>';
+                $response = '<i class="fas fa-times text-danger"></i>';
                 break;
             case "banned":
-                $response = '<i class="fas py-3 fa-ban text-danger"></i>';
+                $response = '<i class="fas fa-ban text-danger"></i>';
                 break;
             case "restricted":
-                $response = '<i class="fas py-3 fa-exclamation text-warning"></i>';
+                $response = '<i class="fas fa-exclamation text-warning"></i>';
                 break;
             case "legal":
-                $response = '<i class="fas py-3 fa-check text-success"></i>';
+                $response = '<i class="fas fa-check text-success"></i>';
                 break;
         }
         return $response;
