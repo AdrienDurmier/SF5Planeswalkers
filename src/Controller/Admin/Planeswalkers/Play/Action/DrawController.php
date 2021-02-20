249a27c3e237983098e32917bc0b93f9547e6240
adrien.durmier <adrien.durmier@amediasolutions.fr>
2021-02-20 10:35:04 +0100

    suite game 12 - draw en post

--- src/Controller/Admin/Planeswalkers/Play/Action/DrawController.php
+++ src/Controller/Admin/Planeswalkers/Play/Action/DrawController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller\Admin\Planeswalkers\Play\Action;
 
+use App\Entity\Planeswalkers\Play\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mercure\PublisherInterface;
 use Symfony\Component\Mercure\Update;
+use App\Service\Planeswalkers\Play\Action\DrawService;
 
 class DrawController extends AbstractController
 {
@@ -15,11 +17,18 @@ class DrawController extends AbstractController
      * @Route("/planeswalkers/play/action/draw", name="planeswalkers.play.action.draw", methods="POST")
      * @param Request $request
      * @param PublisherInterface $publisher
+     * @param DrawService $drawService
      * @return JsonResponse
      */
-    public function draw(Request $request, PublisherInterface $publisher)
+    public function draw(Request $request, PublisherInterface $publisher, DrawService $drawService)
     {
+        $em = $this->getDoctrine()->getManager();
         $datas = $request->request->all();
+
+        // Action
+        $player = $this->getDoctrine()->getRepository(Player::class)->find($datas['player']);
+        $drawService->draw($player, $datas['quantity']);
+        $em->flush();
 
         // Plublication à Mercure
         $topic = 'planeswalkers-game-'.$datas['game'];
--- src/Service/Planeswalkers/Play/HandService.php
+++ src/Service/Planeswalkers/Play/HandService.php
@@ -5,23 +5,21 @@ namespace App\Service\Planeswalkers\Play;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Planeswalkers\Play\Hand;
 use App\Entity\Planeswalkers\Play\Player;
+use App\Service\Planeswalkers\Play\Action\DrawService;
 
 class HandService
 {
     private $em;
-    private $libraryService;
-    private $gameCardHandService;
+    private $drawService;
 
     /**
      * @param EntityManagerInterface $em
-     * @param LibraryService $libraryService
-     * @param GameCardHandService $gameCardHandService
+     * @param DrawService $drawService
      */
-    public function __construct(EntityManagerInterface $em, LibraryService $libraryService, GameCardHandService $gameCardHandService)
+    public function __construct(EntityManagerInterface $em, DrawService $drawService)
     {
         $this->em = $em;
-        $this->libraryService = $libraryService;
-        $this->gameCardHandService = $gameCardHandService;
+        $this->drawService = $drawService;
     }
 
     /**
@@ -34,36 +32,10 @@ class HandService
         $hand->setPlayer($player);
 
         // Pioche 7 cartes de la librairie par défaut
-        $this->draw($player, 7);
+        $this->drawService->draw($player, 7);
 
         $this->em->persist($hand);
         return $hand;
-    }
-
-    /**
-     * @param Player $player
-     * @param int $quantite
-     * @return Hand
-     */
-    public function draw(Player $player, int $quantite): Hand
-    {
-        $hand = $player->getHand();
-        $library = $player->getLibrary();
-
-        for($i=0; $i<$quantite; $i++){
-            // Récupération de la carte du dessus
-            $top_card = $this->libraryService->topCard($library);
-            if ($top_card){
-                // Ajout dans la main
-                $gameCard = $this->gameCardHandService->new($top_card->getCard(), $hand->countGameCardsHand() + 1 );
-                $hand->addGameCardsHand($gameCard);
-                // Suppression dans la librairie
-                $library->removeGameCardsLibrary($top_card);
-            }
-        }
-        $this->em->persist($library);
-
-        return $player->getHand();
     }
 
 }
