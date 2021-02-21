29e69e6431a28c117fbbb4f8994584900f01982a
adrien.durmier <adrien.durmier@amediasolutions.fr>
2021-02-21 18:08:47 +0100

    suite game 22 - nettoyage

--- src/Service/Planeswalkers/Play/HandService.php
+++ src/Service/Planeswalkers/Play/HandService.php
@@ -5,21 +5,21 @@ namespace App\Service\Planeswalkers\Play;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Planeswalkers\Play\Hand;
 use App\Entity\Planeswalkers\Play\Player;
-use App\Service\Planeswalkers\Play\Action\DrawService;
+use App\Service\Planeswalkers\Play\Action\MoveService;
 
 class HandService
 {
     private $em;
-    private $drawService;
+    private $moveService;
 
     /**
      * @param EntityManagerInterface $em
-     * @param DrawService $drawService
+     * @param MoveService $moveService
      */
-    public function __construct(EntityManagerInterface $em, DrawService $drawService)
+    public function __construct(EntityManagerInterface $em, MoveService $moveService)
     {
         $this->em = $em;
-        $this->drawService = $drawService;
+        $this->moveService = $moveService;
     }
 
     /**
@@ -32,7 +32,7 @@ class HandService
         $hand->setPlayer($player);
 
         // Pioche 7 cartes dans library au début d'une partie
-        $this->drawService->draw($player, 7);
+        $this->moveService->draw($player, 7);
 
         $this->em->persist($hand);
         return $hand;
