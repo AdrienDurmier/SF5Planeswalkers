0289aad5ca1b53b3a50052210720e08cfb921288
adrien.durmier <adrien.durmier@amediasolutions.fr>
2021-03-01 22:21:03 +0100

    Game: starting roll dice 4

--- src/Controller/Admin/Planeswalkers/Play/Action/RollDiceController.php
+++ src/Controller/Admin/Planeswalkers/Play/Action/RollDiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin\Planeswalkers\Play\Action;
 
+use App\Entity\Planeswalkers\Play\Game;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,19 +24,37 @@ class RollDiceController extends AbstractController
     {
         $em = $this->getDoctrine()->getManager();
         $datas = $request->request->all();
-        $player = $this->getDoctrine()->getRepository(Player::class)->find($datas['player']);
+
+        $game = $this->getDoctrine()->getRepository(Game::class)->find($datas['game']);
+        $player = $game->getPlayer($this->getUser());
+        $opponent = $game->getOpponent($this->getUser());
+
         $player->setRollDiceStartingGame($datas['face']);
         $em->persist($player);
+
+        $egality = false;
+        if ($opponent->getRollDiceStartingGame() != null){
+            // Si égalité
+            if ($player->getRollDiceStartingGame() == $opponent->getRollDiceStartingGame()){
+                $egality = true;
+            }
+            // Si il a un lancé plus fort qu'un autre
+            if ($player->getRollDiceStartingGame() > $opponent->getRollDiceStartingGame()){
+                $game->setPlayerActive($player);
+            }
+            if ($player->getRollDiceStartingGame() < $opponent->getRollDiceStartingGame()){
+                $game->setPlayerActive($opponent);
+            }
+            $em->persist($game);
+        }
         $em->flush();
 
-        $opponent = $player->getGame()->getOpponent($this->getUser());
-
         // Publication à Mercure
-        $topic = 'planeswalkers-game-'.$player->getGame()->getId();
+        $topic = 'planeswalkers-game-'.$game->getId();
         $datasMercure = [
-            'action'   => 'starting-game-roll-dice',
-            'player'   => $player->getRollDiceStartingGame(),
-            'opponent' => $opponent->getRollDiceStartingGame(),
+            'action'        => 'starting-game-roll-dice',
+            'playerActive'  => $game->getPlayerActive()->getId(),
+            'egality'       => $egality,
         ];
 
         $update = new Update($topic, json_encode($datasMercure));
